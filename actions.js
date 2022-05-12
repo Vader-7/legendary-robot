ca5138b5120b22126e53b3136503a92ff046e8ae
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-05-12 16:25:20 -0400

    XD

---
+++ actions.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+//FUNCIONALIDAD CATALOGO MACETEROS
+    $("#hide1").click(function (e) {
+        e.preventDefault();
+        $("#div1").hide(1000);
+    })
+    $("#hide1").click(function(){
+        $("#descripcion1").show(1000);
+    });
+    $("#catalogo1").click(function (e) {
+        e.preventDefault();
+        $("#div1").show(1000);
+    })
+    $("#catalogo1").click(function (e) {
+        e.preventDefault();
+        $("#descripcion1").hide(1000);
+    })
+//FUNCIONALIDAD CATALOGO SEMILLAS
+    $("#hide2").click(function (e) {
+        e.preventDefault();
+        $("#div1").hide(1000);
+    })
+    $("#hide2").click(function(){
+        $("#descripcion2").show(1000);
+    });
+    $("#catalogo2").click(function (e) {
+        e.preventDefault();
+        $("#div1").show(1000);
+    })
+    $("#catalogo2").click(function (e) {
+        e.preventDefault();
+        $("#descripcion2").hide(1000);
+    })
+//FUNCIONALIDAD CATALOGO FERTILIZANTES
+    $("#hide3").click(function (e) {
+        e.preventDefault();
+        $("#div1").hide(1000);
+    })
+    $("#hide3").click(function(){
+        $("#descripcion3").show(1000);
+    });
+    $("#catalogo3").click(function (e) {
+        e.preventDefault();
+        $("#div1").show(1000);
+    })
+    $("#catalogo3").click(function (e) {
+        e.preventDefault();
+        $("#descripcion3").hide(1000);
+    })
+});
+
