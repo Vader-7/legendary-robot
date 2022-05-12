cf2dc60f43bed770c13468f51bf6ecfcf5767164
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-05-12 17:25:49 -0400

    XD

--- actions.js
+++ actions.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
         e.preventDefault();
         $("#descripcion3").hide(1000);
     })
-//FUNCIONALIDAD MACETEROS
+//FUNCIONALIDAD MACETERO 1
     $("#mac_img1").mouseenter(function (e) {
         e.preventDefault();
         $("#mac_des1").show(1000);
@@ -56,9 +56,47 @@ $(document).ready(function(){
         e.preventDefault();
         $("#mac_img1").hide(1000);
     })
-    $("#mac_img1").mouseleave(function (e) {
+    $("#mac_des1").mouseleave(function (e) {
         e.preventDefault();
         $("#mac_img1").show(1000);
+    })
+    $("#mac_des1").mouseleave(function (e) {
+        e.preventDefault();
+        $("#mac_des1").hide(1000);
+    })
+//FUNCIONALIDAD MACETERO 2
+    $("#mac_img2").mouseenter(function (e) {
+        e.preventDefault();
+        $("#mac_des2").show(1000);
+    })
+    $("#mac_img2").mouseenter(function (e) {
+        e.preventDefault();
+        $("#mac_img2").hide(1000);
+    })
+    $("#mac_des2").mouseleave(function (e) {
+        e.preventDefault();
+        $("#mac_img2").show(1000);
+    })
+    $("#mac_des2").mouseleave(function (e) {
+        e.preventDefault();
+        $("#mac_des2").hide(1000);
+    })
+//FUNCIONALIDAD MACETERO 3
+    $("#mac_img3").mouseenter(function (e) {
+        e.preventDefault();
+        $("#mac_des3").show(1000);
+    })
+    $("#mac_img3").mouseenter(function (e) {
+        e.preventDefault();
+        $("#mac_img3").hide(1000);
+    })
+    $("#mac_des3").mouseleave(function (e) {
+        e.preventDefault();
+        $("#mac_img3").show(1000);
+    })
+    $("#mac_des3").mouseleave(function (e) {
+        e.preventDefault();
+        $("#mac_des3").hide(1000);
     })
 });
 
